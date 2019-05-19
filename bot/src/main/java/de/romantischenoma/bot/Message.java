af2f5cdb0014854d034249b32a42275db98bebaa
IlimSarykbaev <ilim.sarykbaev@gmail.com>
2019-05-19 18:59:44 +0600

    19.05.2019 18.59

---
+++ bot/src/main/java/de/romantischenoma/bot/Message.java
@@ -0,0 +1,278 @@
+package de.romantischenoma.bot;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.regex.Pattern;
+
+import static de.romantischenoma.bot.MessageListActivity.getDataPertanyaan;
+import static de.romantischenoma.bot.MessageListActivity.getDataStopWords;
+import static de.romantischenoma.bot.MessageListActivity.getDataSynonym;
+
+public class    Message {
+    private String message;
+    private User sender;
+    private Date createdAt;
+
+    public Message(String message, User sender) {
+        setMessage(message);
+        setSender(sender);
+        setCreatedAt();
+    }
+
+    String getMessage() {
+        return this.message;
+    }
+
+    User getSender() {
+        return this.sender;
+    }
+
+    Date getCreatedAt() {
+        return this.createdAt;
+    }
+
+
+    void setMessage(String message) {
+        this.message = message;
+    }
+
+    void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    void setCreatedAt() {
+        this.createdAt = Calendar.getInstance().getTime();
+    }
+
+    public static String StringMatching(String message){
+        String question = message;
+        ArrayList<String> dataPertanyaan = getDataPertanyaan();
+        ArrayList<String> dataSynonym = getDataSynonym();
+        ArrayList<String> dataStopWords = getDataStopWords();
+
+        ArrayList<String> alternate = new ArrayList<String>();
+
+        question = FormattingString(dataStopWords, dataSynonym, question);
+
+        final float matchPercentage = (float) 0.9;
+        float persentasekecocokan;
+        String[] temp;
+        boolean found = false;
+        int i;
+        int result;
+        String out = null;
+
+        //Pengecekan langsung satu String dengan algoritma KMP
+        i=0;
+        while(i<dataPertanyaan.size() && !found){
+            temp = dataPertanyaan.get(i).split("\\?\\W");
+            result = KMP(question, FormattingString(dataStopWords, dataSynonym, temp[0]));
+
+            if(result==0){
+                out = temp[1];
+                found = true;
+            }else{
+                i++;
+            }
+
+        }
+        
+        if(!found){
+            //Pengecekan per-substring dengan algoritma BM
+            i=0;
+            float pembilang;    //counter jumlah huruf yang match
+            float pembagi;      //counter jumlah huruf dalam kalimat
+            while(i<dataPertanyaan.size() && !found){
+                String[] subkata = question.split(" ");
+
+                temp = dataPertanyaan.get(i).split("\\?\\W");
+                pembilang=0;
+                pembagi = temp[0].length()-subkata.length+1;
+
+                for(int j=0; j<subkata.length;j++){
+                    result = BM(FormattingString(dataStopWords, dataSynonym, temp[0]),subkata[j]);
+
+                    if(result!=-1){
+                        pembilang += subkata[j].length();
+                    }
+                }
+
+                persentasekecocokan = pembilang/pembagi;
+
+                if((persentasekecocokan)>=matchPercentage){    //presentase kecocokan string
+                    out = temp[1];
+                    found = true;
+                }else{
+                    if((persentasekecocokan)>=0.5){
+                        alternate.add(temp[0]);
+                    }
+                    i++;
+                }
+            }
+        }
+
+        if(!found){ //Pertanyaan tidak ditemukan baik dicari dari kalimat maupun per-kata
+            if(alternate.size()!=0){
+                out = "Apakah yang Anda maksud : \n";
+
+                for(int k=0;k<alternate.size();k++){
+                    if(k==alternate.size()-1){
+                        out = out + "- " + alternate.get(k);
+                    }else{
+                        out = out + "- " + alternate.get(k) + "\n";
+                    }
+
+                }
+            }else{
+                out = "I don't understand, senpai~ Please try a different sentence. >w<";
+            }
+        }
+        return out;
+    }
+
+
+
+    //Fungsi yang mengembalikan string hasil konversi pertanyaan dari user menjadi pernyataan untuk dilakukan pencarian pada list pertanyaan
+    public static String FormattingString(ArrayList<String> stopWords, ArrayList<String> synonym, String kata){
+        kata = kata.replace("\\W?\\?","");     //Menghapus tanda ? pada format pencarian
+        kata = kata.toLowerCase();
+
+        String[] subkata = kata.split(" ");
+
+        for(int i=0;i<stopWords.size();i++){
+            if(cekRegex(kata, stopWords.get(i))){
+                for(int j=0;j<subkata.length;j++){  //Menghapus stopwords pada kata
+                    if(subkata[j]!=stopWords.get(i)){
+                        if(j==0){
+                            kata = subkata[j] + " ";
+                        }else if(j==subkata.length-1){
+                            kata += subkata[j];
+                        }else{
+                            kata = kata + subkata[j] + " ";
+                        }
+                    }
+                }
+            }
+        }
+
+        //Menghapus synonym pada input pertanyaan dengan pencarian kata synonym menggunakan regex
+        for(int i=0;i<synonym.size();i++){
+            String[] temp = synonym.get(i).split("\\=");
+            if(cekRegex(kata, temp[0])){
+                kata = kata.replaceAll(temp[0], temp[1]);
+            }
+        }
+
+        return kata;
+    }
+
+    //Algoritma Pencocokan String Knuth-Morris-Pratt (KMP Algorithm)
+    public static int KMP (String kataInput, String pattern){
+        int n = kataInput.length();
+        int m = pattern.length();
+
+        int fail[] = computeFail(pattern);
+
+        int i = 0;
+        int j = 0;
+
+        while(i<n){
+            if(pattern.charAt(j) == kataInput.charAt(i)){
+                if(j==m-1){
+                    return 0;
+                }
+                i++;
+                j++;
+            }else if(j>0){
+                j = fail[j-1];
+            }else{
+                i++;
+            }
+        }
+
+        return -1;
+    }
+    
+    //Fungsi bagian dari Algoritma KMP
+    public static int[] computeFail(String pattern){
+
+        int fail[] = new int[pattern.length()];
+        fail[0] = 0;
+
+        int m = pattern.length();
+        int i = 1;
+        int j = 0;
+
+        while(i<m){
+            if(pattern.charAt(j)==pattern.charAt(i)){
+                fail[i] = j+1;
+                i++;
+                j++;
+            }else if(j>0){
+                j = fail[j-1];
+            }else{
+                fail[i] = 0;
+                i++;
+            }
+        }
+        return fail;
+    }
+
+    //Algoritma Pencocokan String Boyer-Moore (BM Algorithm)
+    public static int BM(String kataInput, String pattern){
+        int last[] = buildLast(pattern);
+        int n = kataInput.length();
+        int m = pattern.length();
+        int i = m-1;
+
+        if(i>n-1){
+            return -1;
+        }else{
+            int j = m-1;
+            do{
+                if(pattern.charAt(j)==kataInput.charAt(i)){
+                    if(j==0){
+                        return 0;
+                    }else{
+                        i--;
+                        j--;
+                    }
+                }else{
+                    int lo = last[kataInput.charAt(i)];
+                    i = i+m-Math.min(j, 1+lo);
+                    j = m-1;
+                }
+            }while(i<=n-1);
+
+            return -1;
+        }
+    }
+
+    //Fungsi bagian dari Algoritma BM
+    public static int[] buildLast(String pattern){
+
+        int last[] = new int[128];
+
+        for(int i=0;i<128;i++){
+            last[i] = -1;
+        }
+
+        for(int i=0;i<pattern.length();i++){
+            last[pattern.charAt(i)] = i;
+        }
+
+        return last;
+    }
+
+    public static boolean cekRegex(String kataInput, String pattern){
+        boolean cek;
+        String regex = ".*" + pattern + ".*";
+
+        cek = Pattern.matches(regex, kataInput);
+
+        return cek;
+    }
+
+
+}
---
+++ bot/src/main/java/de/romantischenoma/bot/MessageListActivity.java
@@ -0,0 +1,155 @@
+package de.romantischenoma.bot;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+import static de.romantischenoma.bot.Message.StringMatching;
+
+public class MessageListActivity extends AppCompatActivity {
+    private RecyclerView mMessageRecycler;
+    private MessageListAdapter mMessageAdapter;
+    private ArrayList<Message> messageList;
+    private User user;
+    private User pychanUser;
+
+    private Button sendButton;
+    private EditText userChatInput;
+
+    private static ArrayList<String> dataPertanyaan;
+    private static ArrayList<String> dataSynonym;
+    private static ArrayList<String> dataStopWords;
+
+    private static String namaFilePertanyaan = "raw/datapertanyaan.txt";
+    private static String namaFileSynonym = "raw/datasynonym.txt";
+    private static String namaFileStopWords = "raw/datastopwords.txt";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        dataPertanyaan = getDataFromFile(getApplicationContext(), R.raw.datapertanyaan);
+        dataStopWords = getDataFromFile(getApplicationContext(), R.raw.datastopwords);
+        dataSynonym = getDataFromFile(getApplicationContext(), R.raw.datasynonym);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message_list);
+
+        user = new User("kamu");
+        pychanUser = new User("Pychan");
+
+        messageList = new ArrayList<Message>();
+        messageList.add(new Message("Halo, Aku Pychan!", pychanUser));
+        messageList.add(new Message("Aku adalah chatbot yang suka menjawab rasa penasaran dan keisenganmu!", pychanUser));
+        messageList.add(new Message("Eh, sebelumnya, kenalan dulu, dong! Nama kamu siapa?", pychanUser));
+
+        sendButton = findViewById(R.id.button_chatbox_send);
+        userChatInput = findViewById(R.id.edittext_chatbox);
+
+        // SETUP RECYCLERVIEW
+        mMessageRecycler = findViewById(R.id.reyclerview_message_list);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mMessageRecycler.setLayoutManager(layoutManager);
+//        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(mMessageRecycler.getContext(), layoutManager.getOrientation());
+//        mMessageRecycler.addItemDecoration(dividerItemDecoration);
+        mMessageAdapter = new MessageListAdapter(this, messageList);
+        mMessageRecycler.setAdapter(mMessageAdapter);
+
+        setOnClick();
+    }
+
+    private void setOnClick(){
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String content = userChatInput.getText().toString();
+                if (content.trim().length() > 0) {
+                    if (messageList.size() > 3) {
+                        messageList.add(new Message(content, user));
+                        userChatInput.getText().clear();
+                        mMessageAdapter.notifyDataSetChanged();
+                        // Auto scroll when new message is created
+                        mMessageRecycler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                mMessageAdapter.notifyDataSetChanged();
+                                // Call smooth scroll
+                                mMessageRecycler.smoothScrollToPosition(mMessageAdapter.getItemCount());
+                            }
+                        });
+                        new Thread(new Runnable() {
+                            public void run() {
+                                String tempContent = new String(content);
+                                messageList.add(new Message(StringMatching(tempContent), new User("Pychan")));
+                                mMessageRecycler.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        // Call smooth scroll
+                                        mMessageRecycler.smoothScrollToPosition(mMessageAdapter.getItemCount());
+                                        mMessageAdapter.notifyDataSetChanged();
+                                    }
+                                });
+                            }
+                        }).start();
+                        // Auto scroll when new message is created
+                    } else {
+                        messageList.add(new Message(content, user));
+                        userChatInput.getText().clear();
+                        user.setNickname(content);
+                        mMessageAdapter.notifyDataSetChanged();
+                        messageList.add(new Message("Halo, " + user.getNickname() + "! Salam kenal!", pychanUser));
+                        messageList.add(new Message("Kalau ada pertanyaan, langsung tanya aja, ya!", pychanUser));
+                    }
+                }
+
+            }
+        });
+    }
+
+    //Prosedur pembacaan file dan memindahkan ke memori agar data dari file tersimpan
+    public static ArrayList<String> getDataFromFile(Context ctx, int resId){
+
+        ArrayList<String> data = new ArrayList<String>();
+
+        InputStream inputStream = ctx.getResources().openRawResource(resId);
+
+        InputStreamReader inputreader = new InputStreamReader(inputStream);
+        BufferedReader buffreader = new BufferedReader(inputreader);
+
+        String temp = null;
+        StringBuilder text = new StringBuilder();
+            try{
+                while((temp = buffreader.readLine()) != null){
+                    data.add(temp);
+            }
+            System.out.println("BERHASIL");
+//            in.close();
+        }catch(Exception e){
+            System.out.println("Gagal membaca file " + resId);
+//            System.exit(1);
+            e.printStackTrace();
+        }
+
+        return data;
+    }
+
+    public static ArrayList<String> getDataPertanyaan(){
+        return dataPertanyaan;
+    }
+
+    public static ArrayList<String> getDataSynonym(){
+        return dataSynonym;
+    }
+
+    public static ArrayList<String> getDataStopWords(){
+        return dataStopWords;
+    }
+}
