0f9daa76b4db78b32107b20a35ca786fb106d060
IlimSarykbaev <ilim.sarykbaev@gmail.com>
2019-05-31 13:21:56 +0600

    31.05.2019 13.21

--- booking/src/main/java/de/romantischenoma/booking/SecondActivity.java
+++ booking/src/main/java/de/romantischenoma/booking/SecondActivity.java
@@ -3,11 +3,14 @@ package de.romantischenoma.booking;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
     TextView textView;
+    Button button;
     String a;
 
     @Override
@@ -15,11 +18,21 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
          textView = (TextView) findViewById(R.id.business_number);
+         button = (Button) findViewById(R.id.button_booking);
 
          Intent intent = getIntent();
          a = intent.getStringExtra("business number");
 
          textView.setText(a);
 
+         button.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 Intent intentBooking = new Intent(SecondActivity.this, ThirdActivity.class);
+                 startActivity(intentBooking);
+             }
+         });
+
     }
+
 }
